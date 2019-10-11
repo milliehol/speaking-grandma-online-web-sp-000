062547d5c75f389f38ff3ad3c973b681983c80e6
Carla Holcomb <carla.programmer@gmail.com>
2019-10-11 00:51:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-if (phrase.upcase == phrase)
+if (phrase == "I LOVE YOU GRANDMA")
+ return "I LOVE YOU TOO PUMPKIN"
+elsif (phrase.upcase == phrase)
   return "HUH?! SPEAK UP, SONNY!"
 else
   nil
