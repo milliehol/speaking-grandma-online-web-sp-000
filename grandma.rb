70403a7c5dd2bb631edaa1807398121bdbd8d1a5
Carla Holcomb <carla.programmer@gmail.com>
2019-10-11 00:54:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ if (phrase == "I LOVE YOU GRANDMA!")
  return "I LOVE YOU TOO PUMPKIN!"
 elsif (phrase.upcase != phrase)
   return "HUH?! SPEAK UP, SONNY!"
+elsif (phrase.upcase == phrase)
+  return "NO, NOT SINCE 1938!"
 else
   nil
 end
